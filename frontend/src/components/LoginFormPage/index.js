3a7a7222ad8f175f172f97fa64851da554765791
Saad <saad_shaikh@aol.com>
2020-11-19 14:48:49 -0500

    added login form, css needs to be added

---
+++ frontend/src/components/LoginFormPage/index.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import * as sessionActions from "../../store/session";
+import { useDispatch, useSelector } from "react-redux";
+import { Redirect } from "react-router-dom";
+import "./LoginForm.css";
+
+function LoginFormPage() {
+	const dispatch = useDispatch();
+	const sessionUser = useSelector((state) => state.session.user);
+	const [credential, setCredential] = useState("");
+	const [password, setPassword] = useState("");
+	const [errors, setErrors] = useState([]);
+
+	if (sessionUser) return <Redirect to="/" />;
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		setErrors([]);
+		return dispatch(sessionActions.login({ credential, password })).catch(
+			(res) => {
+				if (res.data && res.data.errors) setErrors(res.data.errors);
+			}
+		);
+	};
+
+	return (
+		<div className="container">
+			<form onSubmit={handleSubmit}>
+				<ul>
+					{errors.map((error, idx) => (
+						<li key={idx}>{error}</li>
+					))}
+				</ul>
+				<label className="label">
+					Username or Email
+					<input
+						type="text"
+						className="input"
+						value={credential}
+						onChange={(e) => setCredential(e.target.value)}
+						required
+					/>
+				</label>
+				<label className="label">
+					Password
+					<input
+						type="password"
+						className="input"
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+						required
+					/>
+				</label>
+				<button type="submit">Log In</button>
+			</form>
+		</div>
+	);
+}
+
+export default LoginFormPage;
