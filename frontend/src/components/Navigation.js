95f7e70f50a0603cec33c423a2cf28e0a0a40ddd
Saad <saad_shaikh@aol.com>
2020-11-25 18:12:55 -0500

    fixed the outline bug around the nav logo for the playlist

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -32,7 +32,6 @@ function Navigation({ isLoaded, playlistStatus, setPlaylistStatus }) {
 						onClick={() => setPlaylistStatus(!playlistStatus)}
 					>
 						<img
-							id="logo-img"
 							src="https://www.freelogodesign.org/file/app/client/thumb/2b78cf08-0ba2-48a6-acc4-4eb188527d65_200x200.png?1606005029119"
 							alt="logo"
 						/>
